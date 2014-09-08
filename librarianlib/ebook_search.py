4aa31120748cd2eed8c5517a57cc1245c98ec971
Barsanuphe <paixetprosperite@gmail.com>
2014-09-09 00:00:57 +0200

    Librarian: removed the 'wanted' configuration part because reasons. Search is completely rewritten, can now search all metadata fields available.

--- librarianlib/ebook_search.py
+++ librarianlib/ebook_search.py
@@ -1,106 +1,127 @@
 from collections import defaultdict
+import re
 
 from .epub import Epub, ReadStatus
 
-def fuzzy_search_in_list(searched, string_list, exact = False):
-    for string in string_list:
-        if not exact and searched in string:
-            return True
-        if exact and searched == string:
-            return True
-    return False
-
-def is_ebook_a_match(search_string, ebook_field_list, exact = False):
-    for field in ebook_field_list:
-        if exact and search_string.strip() == field.lower():
-            return True
-        elif not exact and search_string.strip() in field.lower():
-            return True
-    return False
-
-
-class EbookSearch(object):
-
-    def __init__(self, all_ebooks):
-        self.all_ebooks = all_ebooks
-
-    def search_ebooks(self, search_string, exact_search = False):
-        filtered = []
-        search_string = search_string.lower()
-        for eb in self.all_ebooks:
-            if search_string.startswith("series:"):
-                if is_ebook_a_match( search_string.split("series:")[1], eb.metadata.get_values("series"), exact_search):
-                    filtered.append(eb)
-            elif search_string.startswith("author:"):
-                if is_ebook_a_match( search_string.split("author:")[1], eb.metadata.get_values("author"), exact_search):
-                    filtered.append(eb)
-            elif search_string.startswith("title:"):
-                if is_ebook_a_match( search_string.split("title:")[1], eb.metadata.get_values("title"), exact_search):
-                    filtered.append(eb)
-            elif search_string.startswith("tag:"):
-                if fuzzy_search_in_list(search_string.split("tag:")[1].strip(), eb.tags, exact_search):
-                    filtered.append(eb)
-            elif search_string.startswith("progress:"):
-                if fuzzy_search_in_list(ReadStatus[search_string.split("progress:")[1].strip()], [eb.read], True):
-                    filtered.append(eb)
-            elif is_ebook_a_match( search_string, eb.metadata.get_values("series") + eb.metadata.get_values("author") + eb.metadata.get_values("title"), exact_search) or fuzzy_search_in_list(search_string, eb.tags, exact_search):
-                filtered.append(eb)
-        return sorted(filtered, key=lambda x: x.filename)
-
-    def exclude_ebooks(self, ebooks_list, exclude_term):
-        filtered = []
-        exclude_term = exclude_term.lower()
-        for eb in ebooks_list:
-            if exclude_term.startswith("series:"):
-                if not is_ebook_a_match( exclude_term.split("series:")[1], eb.metadata.get_values("series")):
-                    filtered.append(eb)
-            elif exclude_term.startswith("author:"):
-                if not is_ebook_a_match( exclude_term.split("author:")[1], eb.metadata.get_values("author")):
-                    filtered.append(eb)
-            elif exclude_term.startswith("title:"):
-                if not is_ebook_a_match( exclude_term.split("title:")[1], eb.metadata.get_values("title")) :
-                    filtered.append(eb)
-            elif exclude_term.startswith("tag:"):
-                if not fuzzy_search_in_list(exclude_term.split("tag:")[1].strip(), eb.tags):
-                    filtered.append(eb)
-            elif exclude_term.startswith("progress:"):
-                if not fuzzy_search_in_list(ReadStatus[exclude_term.split("progress:")[1].strip()], [eb.read], True):
-                    filtered.append(eb)
-            elif not is_ebook_a_match( exclude_term, eb.metadata.get_values("series") + eb.metadata.get_values("author") + eb.metadata.get_values("title")) and not fuzzy_search_in_list(exclude_term, eb.tags):
-                filtered.append(eb)
-        return sorted(filtered, key=lambda x: x.filename)
-
-    def search(self, search_list, exclude_list, additive=False):
-        complete_filtered_list = []
-
-        if search_list == []:
-            complete_filtered_list = self.all_ebooks
+def list_tags(ebooks):
+    all_tags = defaultdict(lambda: 0)
+    for ebook in ebooks:
+        if ebook.tags == []:
+            all_tags["untagged"] += 1
         else:
-            for library_filter in search_list:
-                # hits for this filter
-                filtered = self.search_ebooks(library_filter)
-                if additive:
-                    # master list if f1 AND f2
-                    if complete_filtered_list == []:
-                        complete_filtered_list = filtered
-                    else:
-                        complete_filtered_list = [el for el in complete_filtered_list if el in filtered]
-                else:
-                    # master list if f1 OR f2
-                    complete_filtered_list.extend([el for el in filtered if el not in complete_filtered_list])
+            for tag in ebook.tags:
+                all_tags[tag] += 1
+    return all_tags
 
-        if exclude_list is not None:
-            for exclude in exclude_list:
-                complete_filtered_list = self.exclude_ebooks(complete_filtered_list, exclude)
+class Search(object):
+    """ This class builds a EvaluateMatch object from input conditions, then loops on all ebooks to pick out the ones who match. """
+    def __init__(self, everything, is_exact = False):
+        self.everything = everything
+        self.is_exact = is_exact
+        self.evaluate_match = EvaluateMatch()
+        self.field_search = re.compile("^([^:]*):(.*)$")
 
-        return sorted(complete_filtered_list, key=lambda x: x.filename)
-
-    def list_tags(self):
-        all_tags = defaultdict(lambda: 0)
-        for ebook in self.all_ebooks:
-            if ebook.tags == []:
-                all_tags["untagged"] += 1
+    def excludes(self, exclude_list):
+        for exclude_term in exclude_list:
+            fields = self.field_search.findall(exclude_term)
+            if fields == []:
+                self.evaluate_match.add_exclude_condition(exclude_term, None, self.is_exact)
             else:
-                for tag in ebook.tags:
-                    all_tags[tag] += 1
-        return all_tags
+                field, value = fields[0]
+                self.evaluate_match.add_exclude_condition(value, field, self.is_exact)
+
+    def filters(self, filter_list):
+         for filter_term in filter_list:
+            fields = self.field_search.findall(filter_term)
+            if fields == []:
+                self.evaluate_match.add_condition(filter_term, None, self.is_exact)
+            else:
+                field, value = fields[0]
+                self.evaluate_match.add_condition(value, field, self.is_exact)
+
+    def run_search(self, and_or): # EvaluateMatch.OR / EvaluateMatch.AND
+        filtered = []
+        for ebook in self.everything:
+            if self.evaluate_match.is_a_match(ebook, and_or):
+                filtered.append(ebook)
+        return filtered
+
+    @property
+    def number_of_results(self):
+        return len(self.filtered)
+
+def match_this(ebook, value, field = None, exact = False):
+    """ Try to see if an Epub object matches the condition given by value, optionnally restricted to a field. """
+    value = value.lower()
+    if field is None:
+        # search everywhere
+        result = False # OR search
+        for key in ebook.metadata.keys:
+            field_value = ebook.metadata.get_values(key)
+            is_list = (type(field_value) == list)
+            if exact:
+                result = result \
+                        or (not is_list and value == field_value.lower()) \
+                        or (is_list     and value in field_value.lower())
+            else:
+                result = result \
+                        or (not is_list and any([(value == val.lower()) for val in field_value])) \
+                        or (is_list     and any([(value in val.lower()) for val in field_value]))
+        # tags
+        if exact:
+            result = result or any([(value == tag.lower()) for tag in ebook.tags])
+        else:
+            result = result or any([(value in tag.lower()) for tag in ebook.tags])
+
+        return result
+
+    else:
+        if field == "tag":
+            if exact:
+                return any([(value == tag.lower()) for tag in ebook.tags])
+            else:
+                return any([(value in tag.lower()) for tag in ebook.tags])
+        else:
+            field_value = ebook.metadata.get_values(field)
+            is_list = (type(field_value) == list)
+            if exact:
+                return (not is_list and value == field_value.lower()) or (is_list and any([(value == val.lower()) for val in field_value]))
+            else:
+                return (not is_list and value in field_value.lower()) or (is_list and any([(value in val.lower()) for val in field_value]))
+
+
+class EvaluateMatch(object):
+    """ This class builds a list of conditions, that are evaluated when is_a_match is passed with an actual Epub object. """
+    OR = 1
+    AND = 2
+
+    def __init__(self):
+        self.full_expression = [] # verif cond de depart, si c'est OR et tout est faux, ça ferait vrai quand meme
+        self.exclude_expression = []
+
+    def add_condition(self, value, field = None, is_exact = False):
+        self.full_expression.append(lambda x: match_this(x, value, field, is_exact))
+
+    def add_exclude_condition(self, value, field = None, is_exact = False):
+        self.exclude_expression.append(lambda x: not match_this(x, value, field, is_exact))
+
+    def _evaluate(self, epub):
+        evaluated =         [ f(epub) for f in self.full_expression]
+        exclude_evaluated = [ f(epub) for f in self.exclude_expression]
+        return evaluated, exclude_evaluated
+
+    def apply_and_condition_to_epub(self, epub):
+        evaluated, exclude_evaluated = self._evaluate(epub)
+        return all(evaluated) and all(exclude_evaluated)
+
+    def apply_or_condition_to_epub(self, epub):
+        evaluated, exclude_evaluated = self._evaluate(epub)
+        return any(evaluated) and all(exclude_evaluated)
+
+    def is_a_match(self, epub, and_or):
+        if and_or == self.AND:
+            return self.apply_and_condition_to_epub(epub)
+        elif and_or == self.OR:
+            return self.apply_or_condition_to_epub(epub)
+        else:
+            print("What?")
